720a0c931245a38cdb064e054b21a1fa84869ffc
Nikolay Khomiakov <nikolay.khomiakov@gmail.com>
2022-08-23 21:39:56 +0300

    add README.md

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,23 +4,22 @@ import pygame
 
 
 class AlienInvasion:
-    '''Класс для управления ресурсами и поведением игры'''
+    """Класс для управления ресурсами и поведением игры"""
 
     def __init__(self):
-        '''Инициализирует игру и создает игровые ресурсы'''
+        """Инициализирует игру и создает игровые ресурсы"""
         pygame.init()
-
         self.screen = pygame.display.set_mode((1200, 800))
-        pygame.display.set_caption(('Alien Invasion'))
+        pygame.display.set_caption('Alien Invasion')
 
     def run_game(self):
-        '''Запуск основного цикла игры'''
+        """Запуск основного цикла игры"""
         while True:
-            ''''Отслеживание событий клавиатуры и мыши'''
+            """Отслеживание событий клавиатуры и мыши"""
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            '''Отображение последнего прорисованного экрана'''
+            """Отображение последнего прорисованного экрана"""
             pygame.display.flip()
 
 
@@ -28,4 +27,3 @@ if __name__ == "__main__":
     '''Создание экземпляра и запуск игры'''
     ai = AlienInvasion()
     ai.run_game()
-
