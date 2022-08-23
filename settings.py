77ee31fd300bb2307e649d07624eebfa0722e99e
Nikolay Khomiakov <nikolay.khomiakov@gmail.com>
2022-08-24 00:19:44 +0300

    ship create

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    """Класс для хранения всех настроек игры"""
+
+    def __init__(self):
+        """Инициирует настройки игры"""
+
+        # Параметры экрана
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
